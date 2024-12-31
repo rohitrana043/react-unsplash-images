ac91d416e437285b8ebc4553e39bd98ccd7efdbd
ROHIT RANA <rohit.rana043@gmail.com>
2024-12-30 20:57:08 -0500

    first commit

---
+++ src/components/SearchForm.jsx
@@ -0,0 +1,28 @@
+import { useGlobalContext } from '../Context';
+
+function SearchForm() {
+  const { setSearchTerm } = useGlobalContext();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const searchValue = e.target.elements.search.value;
+    if (!searchValue) return;
+    setSearchTerm(searchValue);
+  };
+  return (
+    <section>
+      <h1 className="title">unsplash Images</h1>
+      <form className="search-form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className="form-input search-input"
+          name="search"
+          placeholder="cat"
+        />
+        <button className="btn" type="submit">
+          Submit
+        </button>
+      </form>
+    </section>
+  );
+}
+export default SearchForm;
